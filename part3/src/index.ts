24830de2c2419a18e795c3d70439157579fdbdd2
xzt <xzt@qq.com>
2021-10-31 20:38:38 +0800

    first

---
+++ part3/src/index.ts
@@ -0,0 +1,19 @@
+
+function sum(a: number, b: number):number{
+    return  a + b;
+}
+
+var obj = {name:"孙悟空", age:33};
+
+console.log(obj);
+
+obj.age = 18;
+console.log(obj);
+
+console.log(sum(123, 456));
+console.log('哈哈');
+
+let fn = (a: number, b: number) => a + b;
+
+fn(123 , 456);
+fn(77, 22);
